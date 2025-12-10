c6c6f296a0f3af09b53c25d1becb2f5bc97882e1
clement salingue <clement.salingue@ecoles-epsi.net>
2025-12-10 19:00:33 +0100

    mise en place des cases cliquable avec les x qui se mettent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Board from './components/Board.jsx';
+import { useState } from 'react';
 
 export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = 'X';
+    setBoard(newBoard);
+  };
+
   return (
     <div className="text-center mt-6">
       <h1 className="text-3xl font-bold mb-4">Morpion</h1>
-      <Board />
+      <Board board={board} onClick={handleClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,73 @@
-
-
-export default function Board() {
+export default function Board({ board, onClick }) {
   return (
     <div className="mx-auto w-fit mt-8">
-      
       <div className="flex">
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(0)}
+        >
+          {board[0]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(1)}
+        >
+          {board[1]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(2)}
+        >
+          {board[2]}
+        </div>
       </div>
 
-     
       <div className="flex">
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(3)}
+        >
+          {board[3]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(4)}
+        >
+          {board[4]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(5)}
+        >
+          {board[5]}
+        </div>
       </div>
 
-      
       <div className="flex">
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
-        <div className="w-16 h-16 border border-black"></div>
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(6)}
+        >
+          {board[6]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(7)}
+        >
+          {board[7]}
+        </div>
+
+        <div
+          className="w-16 h-16 border flex items-center justify-center text-3xl cursor-pointer"
+          onClick={() => onClick(8)}
+        >
+          {board[8]}
+        </div>
       </div>
     </div>
   );
